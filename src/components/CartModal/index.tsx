c199f7b1f01b0936f8f270edfc168e2818b255cb
caio303 <caioalves_diogo@hotmail.com>
2022-02-11 09:58:06 -0300

    CartModal class component

--- src/components/CartModal/index.tsx
+++ src/components/CartModal/index.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction } from "react"
+import React, { Dispatch, SetStateAction } from "react"
 import { Link } from "react-router-dom"
 import { InCartProductType } from "../../types"
 import { CartItem } from "../CartItem"
@@ -12,61 +12,61 @@ type CartModalProps = {
     setIsCartModalOpen: Dispatch<SetStateAction<boolean>>
 }
 
-export const CartModal = (props: CartModalProps) => {
+export class CartModal extends React.Component<CartModalProps> {
 
-    if(props.myCart.length > 0) {
-        
-        const currencySymbol = props.myCart[0].product.prices[props.currentCurrency].currency.symbol
-        
-        const closeCartModal = () => {
-            props.setIsCartModalOpen(false)
-        }
-
-        return (
-            <Container>
-                
-                <h2>My Bag,&nbsp;<span>{props.myCart.length} items</span></h2>
-                
-                <div id="cartModal-list">
-                    <ul>
-                        {props.myCart.map((item,index) => {
-                            return <CartItem
-                                        key={index}
-                                        liSize="sm"
-                                        itemName={item.product.name}
-                                        itemQuantity={item.quantity}
-                                        currencyAmount={item.product.prices[props.currentCurrency].amount}
-                                        currencySymbol={currencySymbol}
-                                        itemAmount={item.product.prices[props.currentCurrency].amount}
-                                        itemImage={item.product.gallery[0]}
-                                        myCart={props.myCart}
-                                        setMyCart={props.setMyCart}
-                                        currentItem={item}
-                                    />
-                        })}
-                    </ul>
-                    <div id="cartModal-total">
-                        <span>Total</span>
-                        <span>{currencySymbol} {props.cartTotalAmount.toFixed(2)}</span>
-                    </div>
-                    <div id="cartModal-footer">
-                        <Link onClick={closeCartModal} to="/cart">VIEW BAG</Link>
-                        <Link to="/check-out">CHECK OUT</Link>
-                    </div>
-                </div>
-            </Container>
-        )
-    }else {
-        return(
-            <Container>
-                <h2>My Bag,&nbsp;<span>0 items :(</span></h2>
-                <div id="cartModal-list">
-                    <h1 style={{textAlign:"center",padding:"3.9rem 0 3rem 0"}}>Your Bag is <div style={{color: "var(--active-green)"}}>empty!</div></h1>
-                    <h3 style={{textAlign:"center",paddingBottom:".6rem"}}>Take a look at our products!</h3>
-                </div>
-            </Container>
-        )
-    }
-
+    render() {
+        if(this.props.myCart.length > 0) {
+            
+            const currencySymbol = this.props.myCart[0].product.prices[this.props.currentCurrency].currency.symbol
+            
+            const closeCartModal = () => {
+                this.props.setIsCartModalOpen(false)
+            }
     
+            return (
+                <Container>
+                    
+                    <h2>My Bag,&nbsp;<span>{this.props.myCart.length} items</span></h2>
+                    
+                    <div id="cartModal-list">
+                        <ul>
+                            {this.props.myCart.map((item,index) => {
+                                return <CartItem
+                                            key={index}
+                                            liSize="sm"
+                                            itemName={item.product.name}
+                                            itemQuantity={item.quantity}
+                                            currencyAmount={item.product.prices[this.props.currentCurrency].amount}
+                                            currencySymbol={currencySymbol}
+                                            itemAmount={item.product.prices[this.props.currentCurrency].amount}
+                                            itemImage={item.product.gallery[0]}
+                                            myCart={this.props.myCart}
+                                            setMyCart={this.props.setMyCart}
+                                            currentItem={item}
+                                        />
+                            })}
+                        </ul>
+                        <div id="cartModal-total">
+                            <span>Total</span>
+                            <span>{currencySymbol} {this.props.cartTotalAmount.toFixed(2)}</span>
+                        </div>
+                        <div id="cartModal-footer">
+                            <Link onClick={closeCartModal} to="/cart">VIEW BAG</Link>
+                            <Link to="/check-out">CHECK OUT</Link>
+                        </div>
+                    </div>
+                </Container>
+            )
+        }else {
+            return(
+                <Container>
+                    <h2>My Bag,&nbsp;<span>0 items :(</span></h2>
+                    <div id="cartModal-list">
+                        <h1 style={{textAlign:"center",padding:"3.9rem 0 3rem 0"}}>Your Bag is <div style={{color: "var(--active-green)"}}>empty!</div></h1>
+                        <h3 style={{textAlign:"center",paddingBottom:".6rem"}}>Take a look at our products!</h3>
+                    </div>
+                </Container>
+            )
+        }
+    }
 }
